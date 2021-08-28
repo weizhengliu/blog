679b5abfdab767698cfc6cf4554fb69e5647147a
uitsing <tsing@hey.com>
2021-08-28 23:08:08 +0800

    posts

---
+++ assets/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["../lib/*_web/**/*.*ex"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
